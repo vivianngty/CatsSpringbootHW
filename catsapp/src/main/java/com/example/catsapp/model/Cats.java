fcce928e5b9b0a6ee97e4149edf412ed22450ad7
Vivian Ng <vivian.ng.t.y@gmail.com>
2021-02-12 13:53:15 -0500

    create cat model with constructor, getter and setter

---
+++ catsapp/src/main/java/com/example/catsapp/model/Cats.java
@@ -0,0 +1,61 @@
+package com.example.catsapp.model;
+
+public class Cats {
+    private Long id;
+    private String name;
+    private String breed;
+    private String color;
+    private int age;
+    private String ownersName;
+
+    public Cats() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getOwnersName() {
+        return ownersName;
+    }
+
+    public void setOwnersName(String ownersName) {
+        this.ownersName = ownersName;
+    }
+}
